19d3829de960cf903992537982952f721c2f4178
Alexandru Iatan <alex@ccllhr.com>
2018-06-05 18:19:55 +0100

    lesson 1-4

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -18,12 +18,38 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
 
 
 
 
 #########################################################
 ### your code goes here ###
+
+from sklearn.svm import SVC
+
+clf = SVC(C=10000, kernel="rbf")
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(pred, labels_test)
+
+print "accuracy: ", accuracy
+
+chrisEmails = 0
+
+for index in range(len(pred)):
+	if pred[index] == 1:
+		chrisEmails += 1
+		
+print "chrisEmails: " , chrisEmails
 
 #########################################################
 
