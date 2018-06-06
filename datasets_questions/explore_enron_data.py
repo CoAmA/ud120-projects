06f4fa753aac9606a8ff510a9e01ba49aa3200e9
CoAmA <alexandrubogdan.iatan@gmail.com>
2018-06-06 20:27:35 +0000

    Fig Added2?

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -15,8 +15,43 @@
     
 """
 
+from pprint import pprint
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+countSalary = 0
+countEmail = 0
+countPOI = 0
+countTPPOI = 0
+count = 0
+for intex,person in enron_data.iteritems():
+	print intex
+#	if enron_data[person]["salary"] != 'NaN':
+#		countSalary = countSalary+1
+#	if enron_data[person]["email_address"] != 'NaN':
+#		countEmail = countEmail+1
+#	if enron_data[person]["poi"] == True:
+#		if enron_data[person]["total_payments"] == 'NaN':
+#			countTPPOI = countTPPOI+1
+#		countPOI = countPOI+1
+#	count = count+1
+#print countTPPOI
+#print countPOI
+#print count
+#print (countTPPOI*100)/countPOI
 
+#print countSalary
+#print countEmail
+#print count
+
+#pprint(enron_data)
+
+#data = len(open("../final_project/poi_names.txt").readlines())
+#print data + count
+#print(len(open("../final_project/poi_names.txt").readlines()))
+#pprint(enron_data["PRENTICE JAMES"])
+#pprint(enron_data["COLWELL WESLEY"])
+#list = [enron_data["SKILLING JEFFREY K"]["total_payments"],enron_data["LAY KENNETH"]["total_payments"]]
+#pprint(list)
+#pprint(enron_data["LAY KENNETH LEE"])
