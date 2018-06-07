b2285f75964d6601f316e55a7fb1f6343be49cb5
CoAmA <alexandrubogdan.iatan@gmail.com>
2018-06-07 11:42:32 +0000

    outliers without total

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -4,24 +4,22 @@ import pickle
 import sys
 import matplotlib
 matplotlib.use("Agg")
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
+from pprint import pprint
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+data_dict.pop("TOTAL",0)
 data = featureFormat(data_dict, features)
-
 
 ### your code below
 for point in data:
-    salary = point[0]
-    bonus = point[1]
-    matplotlib.pyplot.scatter( salary, bonus )
-matplotlib.pyplot.xlabel("salary")
-matplotlib.pyplot.ylabel("bonus")
-matplotlib.pyplot.savefig("temp.png")
-
-
+	salary = point[0]
+	bonus = point[1]
+	plt.scatter(salary, bonus)
+plt.xlabel("salary")
+plt.ylabel("bonus")
+plt.savefig("outlierswithout_total.png")
