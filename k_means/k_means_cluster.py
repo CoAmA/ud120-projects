18bc86d1b9e2f6b8bdde939b0b054d953c958c23
root <root@alex2.ccllhr.net>
2018-06-07 15:07:53 +0100

    fifth k_means commit

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -25,14 +25,14 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     ### plot each cluster with a different color--add more colors for
     ### drawing more than five clusters
     colors = ["b", "c", "k", "m", "g"]
-    for ii, pp in enumerate(pred):
-        plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
+    for iii, pp in enumerate(pred):
+        plt.scatter(features[iii][0], features[iii][1], color = colors[pred[iii]])
 
     ### if you like, place red stars over points that are POIs (just for funsies)
     if mark_poi:
-        for ii, pp in enumerate(pred):
-            if poi[ii]:
-                plt.scatter(features[ii][0], features[ii][1], color="r", marker="*")
+        for iii, pp in enumerate(pred):
+            if poi[iii]:
+                plt.scatter(features[iii][0], features[iii][1], color="r", marker="*")
     plt.xlabel(f1_name)
     plt.ylabel(f2_name)
     plt.savefig(name)
@@ -50,18 +50,12 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
-kmeans = km(n_clusters = 2)
-salary = []
-pois = []
-stock_opts = []
-for point in data:
-	pois.append(point[0])
-	salary.append(point[1])
-	stock_opts.append(point[2])
+kmeans = km(n_clusters = 3)
 
 pred = kmeans.fit_predict(finance_features)
 #pprint(kmeans.cluster_centers_)
@@ -70,8 +64,8 @@ pred = kmeans.fit_predict(finance_features)
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
-    plt.scatter( f1, f2 )
+for f1, f2, f3 in finance_features:
+    plt.scatter( f1, f2, f3 )
 plt.show()
 plt.savefig("k_means.png")
 
