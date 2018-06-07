dd8c2e3692893e298cd9c0a0796c149fc8bfd965
root <root@alex2.ccllhr.net>
2018-06-07 15:22:46 +0100

    8th k_means commit

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -3,10 +3,6 @@
 """ 
     Skeleton code for k-means clustering mini-project.
 """
-
-
-
-
 import pickle
 import numpy
 import matplotlib
@@ -17,8 +13,6 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from sklearn.cluster import KMeans as km
 
-
-
 def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature 1", f2_name="feature 2"):
     """ some plotting code designed to help you visualize your clusters """
 
@@ -26,13 +20,13 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     ### drawing more than five clusters
     colors = ["b", "c", "k", "m", "g"]
     for ii, pp in enumerate(pred):
-        plt.scatter(features[ii][0], features[ii][1], features[ii][2], color = colors[pred[ii]])
+        plt.scatter(features[ii][0], features[ii][1], color = colors[pred[ii]])
 
     ### if you like, place red stars over points that are POIs (just for funsies)
     if mark_poi:
         for ii, pp in enumerate(pred):
             if poi[ii]:
-                plt.scatter(features[ii][0], features[ii][1], features[ii][2], color="r", marker="*")
+                plt.scatter(features[ii][0], features[ii][1], color="r", marker="*")
     plt.xlabel(f1_name)
     plt.ylabel(f2_name)
     plt.savefig(name)
@@ -55,7 +49,7 @@ poi  = "poi"
 features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
-kmeans = km(n_clusters = 3)
+kmeans = km(n_clusters = 2)
 
 pred = kmeans.fit_predict(finance_features)
 #pprint(kmeans.cluster_centers_)
@@ -64,8 +58,8 @@ pred = kmeans.fit_predict(finance_features)
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, f3 in finance_features:
-    plt.scatter( f1, f2, f3 )
+for f1, f2, _ in finance_features:
+    plt.scatter( f1, f2 )
 plt.show()
 plt.savefig("k_means.png")
 
