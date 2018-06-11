77e03a7a3f8fbb6d4857e232f9e990799fcd72e2
root <root@alex2.ccllhr.net>
2018-06-11 17:34:46 +0100

    Lesson 14 Validation, validation folder

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,6 +14,7 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import cross_validation
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -24,9 +25,14 @@ features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
-
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
 
 
 ### it's all yours from here forward!  
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score as ac
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print ac(labels_test, clf.predict(features_test))
 
